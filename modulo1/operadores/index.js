10d2086188f6b2593505f67311df29f012da8d47
Andrei Souza Santos <123andrei.santos@gmail.com>
2022-05-10 23:12:15 -0300

    Aula operadores

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,20 @@
+//Exercício de interpretação de código 01:
+/*
+a. false
+b. false
+c. true
+d. boolean
+*/
+
+
+//Exercício de interpretação de código 02 e 03:
+/*
+Quando o programa pede para inserir os 2 números, esses valores são recebidos como string 
+causando problema de concatenação.
+
+Uma das formas de resolver isso é usando o comando Number() antes do prompt, exemplo:
+
+let primeiroNumero = Number(prompt("Digite um numero!"))
+let segundoNumero = Number(prompt("Digite outro numero!"))
+*/
+
